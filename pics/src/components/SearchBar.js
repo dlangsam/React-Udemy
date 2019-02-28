b478da7f686b88f8c43b21c6d5a47653b9f9c965
Devorah Langsam <devorah@langsam.org>
2019-02-28 09:09:04 -0500

    Look for photos and show them

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    render() {
-    return (
-     <div className="ui segment"> 
-        <form className="ui form">
-            <div className="field">
-                <label>Image Search</label>
-                <input type="text"
-/>  
-            </div>
-      </form>
-     </div>
-    );
 
-}
+    state = {term: ''};
+
+    onFormSubmit = event => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    };
+
+    render() {
+        return (
+         <div className="ui segment"> 
+            <form onSubmit={this.onFormSubmit} className="ui form">
+                <div className="field">
+                    <label>Image Search</label>
+                    <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value})} />  
+                </div>
+          </form>
+         </div>
+        );
+
+    }
 
 };
 
